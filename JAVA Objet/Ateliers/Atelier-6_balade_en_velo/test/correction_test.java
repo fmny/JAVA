45c46a35fb21ea9723c6796f38890b5d2cba5569
Francis Mauny <70910795+fmny@users.noreply.github.com>
2022-04-15 15:32:14 +0200

    Add files via upload

---
+++ JAVA Objet/Ateliers/Atelier-6_balade_en_velo/test/correction_test.java
@@ -0,0 +1,11 @@
+package test;
+
+import bo.Magasin;
+
+public class correction_test {
+	public static void main(String[] args) {
+		
+		Magasin monMagasin= new Magasin();
+		System.out.println(monMagasin.toString());
+	}
+}
