d5694ed29428bf940286dc4655b0e9822079a498
Francis Mauny <70910795+fmny@users.noreply.github.com>
2022-07-03 14:38:16 +0200

    Add files via upload

---
+++ JAVA Objet/Ateliers/Atelier-6_balade_en_velo/correction/TP6_Cycles_correction/src/test/TestCycle.java
@@ -0,0 +1,10 @@
+package test;
+
+import bo.Magasin;
+
+public class TestCycle {
+	public static void main(String[] args) {
+		Magasin monMagasin = new Magasin();
+		System.out.println(monMagasin.toString());
+	}
+}
